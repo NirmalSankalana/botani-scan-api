719a000021d8410ee90fbb6e35bf60b0d1e20100
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-11-18 00:34:32 +0530

    Add ORM Models

--- app/models.py
+++ app/models.py
@@ -5,31 +5,47 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 
-class Post(Base):
-    __tablename__ = "posts"
-    id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='True', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), nullable=False)
-    owner = relationship("User")  # Fetches relationship data for us
-
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    user_type = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
 
 
-class Vote(Base):
-    __tablename__ = "votes"
-    user_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), primary_key=True)
-    post_id = Column(Integer, ForeignKey(
-        "posts.id", ondelete="CASCADE"), primary_key=True)
+class Crop(Base):
+    __tablename__ = "crops"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    image_id = Column(Integer, nullable=False)
+
+
+class Disease(Base):
+    __tablename__ = "diseases"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    image_id = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    crop_id = Column(Integer, ForeignKey(
+        "crops.id", ondelete="CASCADE"), nullable=False)
+    crop = relationship("Crop")  # Fetches relationship data for us
+
+
+class Remedy(Base):
+    __tablename__ = "remedies"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
+    image_id = Column(Integer, nullable=False)
+    disease_id = Column(Integer, ForeignKey(
+        "diseases.id", ondelete="CASCADE"), nullable=False)
+    disease = relationship("Disease")  # Fetches relationship data for us
