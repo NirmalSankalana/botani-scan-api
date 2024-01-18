dc21a3a2ecf755c8c1a7c7ce3cb31f1ae7bd0d43
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2024-01-18 21:56:55 +0530

    add data file

---
+++ data.js
@@ -0,0 +1,11 @@
+{
+  "email": "surani@gmail.com",
+  "password": "GoodChild@123",
+  "user_type": "admin"
+}
+
+{
+    "email": "sankalana.nirmal@gmail.com",
+    "password": "Nirmal@1234",
+    "user_type": "user"
+}
